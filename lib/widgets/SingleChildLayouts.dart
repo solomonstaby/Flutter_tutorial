2e7f660cc537d885c2571395873ed9afcd65c230
solomonstaby <solu27000@gmail.com>
2021-09-23 22:53:19 +0530

    Adding again

---
+++ lib/widgets/SingleChildLayouts.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SingleChildLayouts extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Padding(padding: EdgeInsets.all(100),
+    child: Text("Solomon"));
+  }
+}
+
+//Center
+//SizedBox
+//Align
